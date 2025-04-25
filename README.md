86653d1092e2e9ea6b32527060b419b4402f8e58
josh <jtlsc7@gmail.com>
2025-04-25 00:50:36 -0400

    Finished P9

---
+++ README.md
@@ -0,0 +1,2 @@
+My solutions for Advent of Code 2024 (a bit late lol)
+Many are unoptimized, I'm just trying to find the solutions.
--- p9.cpp
+++ p9.cpp
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <map>
 #include <numeric>
+#include <set>
 #include <tuple>
 
 using namespace std;
@@ -79,51 +80,26 @@ struct Block {
 	}
 };
 
-tuple<vector<string>, map<int, int>, map<int, int>> generate_memmap_vec_chunk(const string& in) {
-	vector<string> first_out;
-	map<int, int> second_out;
-	map<int, int> third_out;
+tuple<vector<string>, set<string>> generate_memmap_vec_chunk(const string& in) {
+	vector<string> memmap;
+	set<string>    found_numbers;
 	for (int i = 0; i < in.size(); ++i) {
 		if (i % 2 == 0) {
 			for (int j = 0; j < stoi(string(1, in[i])); ++j) {
-				//if (j == 0) temp_str = to_string(i / 2);
-				first_out.push_back(to_string(i / 2));
+				memmap.push_back(to_string(i / 2));
 			}
-			//second_out[i + stoi(string(1, in[i]))] = i;
+			found_numbers.insert(to_string(i / 2));
 		}
 		else {
 			for (int j = 0; j < stoi(string(1, in[i])); ++j) {
-				first_out.push_back(".");
+				memmap.push_back(".");
 			}
-			//third_out[i] = i + stoi(string(1, in[i]));
 		}
 	}
-
-	int i_c = 0;
-	int j_c = 0;
-	bool ten = false;  // Won't work since there can be multiple things like 9996
-	if (first_out[0][0] == '.') ten = true;
-	for (auto w : first_out) {
-		if (ten && w != ".") {
-			third_out[j_c] = i_c;
-			ten = false;
-			++i_c;
-			j_c = i_c;
-		}
-		else if (!ten && w == ".") {
-			second_out[i_c] = j_c;
-			ten = true;
-			++i_c;
-			j_c = i_c;
-		}
-		else {
-			++i_c;
-		}
-	}
-	return { first_out, second_out, third_out };
+	return { memmap, found_numbers};
 }
 
-vector<string> move_blocks_vec_chunk(vector<string> in, map<int, int> block, map<int, int> empty) {
+vector<string> move_blocks_vec_chunk(vector<string> in, set<string> found_nums) {
 	auto l = in.begin();
 	auto r = in.end();
 	--r;
@@ -138,41 +114,73 @@ vector<string> move_blocks_vec_chunk(vector<string> in, map<int, int> block, map
 		}
 		};
 
-	// starts with r pointing at the end of the block.
-	auto block_swap = [&block, &empty, &in](auto l, auto r) -> bool {
-		// find r begin and l end;
-		int r_idx = static_cast<int>(distance(begin(in), r));
-		int r_start = block[r_idx];
-		int l_end = empty[static_cast<int>(distance(begin(in), l))];
-
-		while (l < r) {
-			int l_idx = static_cast<int>(distance(begin(in), l));
-			if (empty.contains(l_idx) && block.contains(r_idx)) {
-				if ((empty[l_idx] - l_idx) >= (r_idx - r_start)) {
-					// actually move it.
-					while (distance(begin(in), r) >= r_start) {
-						iter_swap(l, r);
-						++l;
-						--r;
-					}
-					return true;
-				}
-			}
+	auto gap_size = [&in](auto l) -> int {
+		// Assume on start of gap.
+		int count = 0;
+		while (*l == ".")
+		{
+			++count;
 			++l;
 		}
+		return count;
+		};
+
+	auto value_size = [&in](auto r, string c) -> int {
+		// Assume on end of value list
+		int count = 0;
+		while (*r == c) {
+			++count;
+			--r;
+		}
+		return count;
+		};
+
+	auto find_next_gap = [&in](auto l) -> vector<string>::iterator {
+		while (l < end(in) && *l == ".") ++l;
+		while (l < end(in) && *l != ".") ++l;
+		return l;
+		};
+
+	// starts with r pointing at the end of the block.
+	auto block_swap = [&in](auto l, auto r, string swap_char) {
+		while (l < r && *r == swap_char) {
+			iter_swap(l, r);
+			++l;
+			--r;
+		}
+		};
+
+	auto find_first_match = [&in, &gap_size, &value_size, &find_next_gap, &block_swap](vector<string>::iterator l, vector<string>::iterator r) -> bool {
+		while (l < r) {
+			int l_size = gap_size(l);
+			int r_size = value_size(r, *r);
+			if (l_size >= r_size) {
+				block_swap(l, r, *r);
+				return true;
+			}
+			auto temp = find_next_gap(l);
+			if (temp == l) break;
+			l = temp;
+		}
 		return false;
+		};
+
+	auto find_next_good_value = [&in, &found_nums](auto r) -> vector<string>::iterator {
+		if (found_nums.contains(*r)) return r;
+		while (r > begin(in) && !found_nums.contains(*r)) --r;
+		return r;
 		};
 
 	find_next();
 
 	while (l < r) {
-		if (!block_swap(l, r)) {
-			--r;
+		string temp_r = *r;
+		if (found_nums.contains(*r)) {
+			find_first_match(l, r);
+			found_nums.erase(temp_r);
 		}
-		//++l;
-		//--r;
-		find_next();
-		vector_print(in);
+		r = find_next_good_value(r);
+		if (*l != ".") l = find_next_gap(l);
 	}
 	return in;
 }
@@ -180,19 +188,9 @@ vector<string> move_blocks_vec_chunk(vector<string> in, map<int, int> block, map
 string P9::second_solver(string path) {
 	vector<string> data = read_file(data_path / filesystem::path(path));
 	if (data.size() > 1) return "error";
-	auto [memmap, block_size, empty_size] = generate_memmap_vec_chunk(data[0]);
-	vector_print(memmap);
-	for (auto stuff : block_size) {
-		print(to_string(stuff.second) + "," + to_string(stuff.first));
-	}
-	print("=================");
-	for (auto stuff : empty_size) {
-		cout << stuff.first << "," << stuff.second << endl;
-	}
-	//vector<string> blocks = move_blocks_vec_chunk(memmap, block_size, empty_size);
-	print("==================");
-	//vector_print(blocks);
-	return path;
+	auto [memmap, found_nums] = generate_memmap_vec_chunk(data[0]);
+	vector<string> blocks = move_blocks_vec_chunk(memmap, found_nums);
+	return to_string(update_checksum_vec(blocks));
 }
 
 string P9::test_first() {
